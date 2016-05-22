03f328c1ba27262fca002dcfbc3e68ea77d67f75
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-05-21 18:00:11 -0600

    updated cannon barrel tests

--- test/cannon_barrel_test.js
+++ test/cannon_barrel_test.js
@@ -5,6 +5,26 @@ const CannonBase = require('../lib/cannon_base');
 const CannonBarrel = require('../lib/cannon_barrel');
 
 describe('CannonBarrel', function() {
+  context('with assigned and default attributes', function() {
+    var cannonBase = new CannonBase({x: 10, y:10});
+
+    it('should assign an x coordinate', function() {
+      assert.equal(cannonBase.x, 10);
+    });
+
+    it('should assign an y coordinate', function() {
+      assert.equal(cannonBase.y, 10);
+    });
+
+    it('should assign an width coordinate', function() {
+      assert.equal(cannonBase.width, 40);
+    });
+
+    it('should assign an height coordinate', function() {
+      assert.equal(cannonBase.height, 40);
+    });
+  });
+  
   context('can move up and down', function() {
     var cannonBase = new CannonBase({x: 10, y:10});
     var cannonBarrel = new CannonBarrel(cannonBase);
