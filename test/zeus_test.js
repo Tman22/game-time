66a2c58302c6c09adce6e26303a32d72308d19e3
Taylor Moore <Tmoore2272@gmail.com>
2016-05-25 12:53:25 -0600

    Adds more test for zeus

--- test/zeus_test.js
+++ test/zeus_test.js
@@ -5,10 +5,16 @@ var collision = require('../lib/collision');
 
 var Zeus = new Zeus();
 var cannonBarrel = {x: 10, y: 10, anglex: 20, angley: 25};
-var toggle = {x: 5, y: 15};
+var toggle = {x: 5, y: 15, pause: true};
+var ball = {x: 5, y: 6, width: 15, height: 8};
+var target = {x: 20, y: 6, width: 50, height: 50};
+var target2 = {x: 50, y: 6, width: 50, height: 50};
 
-Zeus.cannonBarrel = cannonBarrel
-Zeus.toggle = toggle
+Zeus.cannonBarrel = cannonBarrel;
+Zeus.toggle = toggle;
+Zeus.projectiles.push(ball);
+Zeus.targets.push(target);
+Zeus.targets.push(target2);
 
 describe('Zeus', function() {
   context('with assigned and default attributes', function() {
@@ -27,7 +33,6 @@ describe('Zeus', function() {
       Zeus.cannonBarrel.y = 20
 
       var coords = Zeus.setCoordinates();
-
       assert.equal(coords.x, 20);
       assert.equal(coords.y, 20);
       assert.equal(coords.anglex, 20);
@@ -36,11 +41,91 @@ describe('Zeus', function() {
     });
   });
 
-  context('projectile', function() {
-    it('sets and returns an object with coordinates', function() {
+  context('projectile collision', function() {
+    it('projectile collides with target and both pop()', function() {
+      Zeus.projectileCollidesWithTarget();
 
-
+      assert.equal(Zeus.projectiles.length, 0);
+      assert.equal(Zeus.targets.length, 1);
+      assert.equal(Zeus.toggle.pause, false);
     });
+
+    it('projectile pops when it hits a tower', function() {
+      Zeus.projectiles.push(ball);
+      Zeus.towers.push(target);
+      Zeus.towers.push(target2);
+      Zeus.toggle.pause = true;
+
+      Zeus.projectileCollidesWithTower();
+
+      assert.equal(Zeus.projectiles.length, 0);
+      assert.equal(Zeus.towers.length, 2);
+      assert.equal(Zeus.toggle.pause, false);
+    });
+
+    it('projectile is greater then canvas.y', function() {
+      ball.y = 801;
+      Zeus.projectiles.push(ball);
+      Zeus.toggle.pause = true;
+
+      Zeus.projectileCollidesWithTower();
+
+      assert.equal(Zeus.projectiles.length, 0);
+      assert.equal(Zeus.toggle.pause, false);
+    });
+
+    it('projectile is inside canvas.y', function() {
+      ball.y = 800;
+      Zeus.projectiles.push(ball);
+      Zeus.toggle.pause = true;
+
+      Zeus.projectileCollidesWithTower();
+
+      assert.equal(Zeus.projectiles.length, 1);
+      assert.equal(Zeus.toggle.pause, true);
+    });
+
+    it('projectile is greater then canvas.x', function() {
+      ball.y = 6;
+      ball.x = 801
+      Zeus.projectiles.push(ball);
+      Zeus.toggle.pause = true;
+
+      Zeus.projectileCollidesWithTower();
+
+      assert.equal(Zeus.projectiles.length, 0);
+      assert.equal(Zeus.toggle.pause, false);
+    });
+
+    it('projectile is less then canvas.x', function() {
+      ball.x = -1;
+      Zeus.projectiles.push(ball);
+      Zeus.toggle.pause = true;
+
+      Zeus.projectileCollidesWithTower();
+
+      assert.equal(Zeus.projectiles.length, 0);
+      assert.equal(Zeus.toggle.pause, false);
+    });
+
+    it('projectiles is an [] - tower collision', function() {
+      Zeus.toggle.pause = true;
+
+      Zeus.projectileCollidesWithTower();
+
+      assert.equal(Zeus.projectiles.length, 0);
+      assert.equal(Zeus.toggle.pause, true);
+    });
+
+    it('projectiles is an [] - targets collision', function() {
+      Zeus.toggle.pause = true;
+
+      Zeus.projectileCollidesWithTarget();
+
+      assert.equal(Zeus.projectiles.length, 0);
+      assert.equal(Zeus.toggle.pause, true);
+    });
+
   });
 
 });
